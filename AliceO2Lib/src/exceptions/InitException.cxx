dc10f778fe10449926fe459f66a189514335dbae
Heiko van der Heijden <heikovdheijden@hotmail.com>
2017-09-27 14:31:15 +0200

    removed old folder of AliceO2Lib

--- AliceO2Lib/src/exceptions/InitException.cxx
+++
@@ -1,7 +0,0 @@
-#include "O2/exceptions/InitException.h"
-
-using namespace O2::Exceptions;
-
-InitException::InitException(const std::string& msg) : AbstractException(msg){
-    
-}
--- AliceO2Lib/src/exceptions/UnimplementedException.cxx
+++
@@ -1,8 +0,0 @@
-#include "O2/exceptions/UnimplementedException.h"
-
-using namespace O2::Exceptions;
-
-
-UnimplementedException::UnimplementedException(const std::string& msg) : AbstractException(msg){
-    
-}
