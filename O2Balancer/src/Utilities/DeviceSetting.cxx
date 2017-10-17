8009b89dd45cfa3c9504a331c27c6e5e53c6b5c4
Your Name <you@example.com>
2017-10-17 10:56:11 +0000

    fixed bug

--- O2Balancer/src/Utilities/DeviceSetting.cxx
+++ O2Balancer/src/Utilities/DeviceSetting.cxx
@@ -1,6 +1,7 @@
 #include "O2/Balancer/Utilities/DeviceSetting.h"
 #include <regex>
 #include <iostream>
+#include <boost/algorithm/string.hpp>
 using namespace O2::Balancer;
 
 DeviceSetting::DeviceSetting(int port, std::string ip){
@@ -8,12 +9,24 @@ DeviceSetting::DeviceSetting(int port, std::string ip){
     this->ip = ip;
 }
 
-const std::regex parseSetting("([^:]+):([0-9]+)", std::regex_constants::ECMAScript);
+
+//const std::regex parseSetting("([^:]+):([0-9]+)", std::regex_constants::ECMAScript);
 
 DeviceSetting::DeviceSetting(const std::string& setting){
-    std::smatch match;
-    std::regex_match(setting, match, parseSetting);
-    this->ip = match[1];
-    this->port = std::stoi(match[2]);
+    try{
+        const std::regex parseSetting("([^:]+):([0-9]+)", std::regex_constants::ECMAScript);
+	std::smatch match;
+
+    	std::regex_match(setting, match, parseSetting);
+    	this->ip = match[1];
+    	this->port = std::stoi(match[2]);
+
+    
+    } catch(const std::regex_error& error){
+	std::vector<std::string> x;
+	boost::split(x, setting, boost::is_any_of(":"));
+	this->ip = x[0];
+	this->port = std::stoi(x[1]);
+    }
 }
 
